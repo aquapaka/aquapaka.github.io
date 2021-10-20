0cf19289e0398f4e9d8a5e293e379ae37c1d47d6
Aquapaka <aqua.tamlong@gmail.com>
2021-10-20 23:59:01 +0700

    Changes

--- js/script.js
+++ js/script.js
@@ -17,12 +17,14 @@ $(document).ready(function () {
             $("#avatar1").show();
             $("#welcome").text("Hi, I'm Meimei");
             $("body").css("background-color", "#FFBF00");
+            $(".pixel-border").css("background-color", "#f7d264");
             changed = true;
         } else {
             $("#avatar1").hide();
             $("#avatar").show();
             $("#welcome").text("Hi, I'm Aquapaka");
             $("body").css("background-color", "#03A9F4");
+            $(".pixel-border").css("background-color", "#60c4f3");
             changed = false;
         }        
     }
