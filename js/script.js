fd3420dc146b4dc4d9c6beaf7cfabc033ff001bf
Aquapaka <aqua.tamlong@gmail.com>
2021-10-20 21:56:05 +0700

    Change style

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
     let clickCount = 0;
     let changed = false;
 
-    $("#avatar").click(function (e) {
+    $("#avatar, #avatar1").click(function (e) {
         e.preventDefault();
         clickCount++;
         console.log(clickCount);
@@ -15,12 +15,14 @@ $(document).ready(function () {
 
     function changeAvatar() {
         if(!changed) {
-            $("#avatar").attr("src", "img/My Love Pixel Art 800x800.png");
-            $("#welcome").text("Hi, I'm Meimei <3");
+            $("#avatar").hide();
+            $("#avatar1").show();
+            $("#welcome").text("Hi, I'm Meimei !");
             $("body").css("background-color", "#FFBF00");
             changed = true;
         } else {
-            $("#avatar").attr("src", "img/Aquatixel Pixel Art 800x800.png");
+            $("#avatar1").hide();
+            $("#avatar").show();
             $("#welcome").text("Hi, I'm Aquapaka");
             $("body").css("background-color", "#03A9F4");
             changed = false;
