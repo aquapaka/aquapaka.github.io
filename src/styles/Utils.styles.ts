441c777ceace5ff4fffe2ea7aa236db42ff41fbe
aquapaka <aqua.tamlong@gmail.com>
2023-01-11 20:59:39 +0700

    Fixing hover stuck on mobile

--- src/styles/Utils.styles.ts
+++ src/styles/Utils.styles.ts
@@ -1,13 +1,13 @@
 import styled from "styled-components";
 
 export const DropShadowImageButton = styled.img`
-  & {
-    filter: drop-shadow(4px 4px rgba(0, 0, 0, 0.7));
-  }
+  filter: drop-shadow(4px 4px rgba(0, 0, 0, 0.7));
 
-  &:hover {
-    filter: drop-shadow(8px 8px rgba(0, 0, 0, 0.7));
-    transform: translate(-2px, -2px);
+  @media (hover: hover) {
+    &:hover {
+      filter: drop-shadow(8px 8px rgba(0, 0, 0, 0.7));
+      transform: translate(-2px, -2px);
+    }
   }
 
   &:active {
@@ -17,19 +17,19 @@ export const DropShadowImageButton = styled.img`
 `;
 
 export const DropShadowNormalButton = styled.a`
-  & {
-    display: ${props => props.href ? "inline-block" : "none"};
-    color: #000;
-    text-decoration: none;
-    background: #fff;
-    margin: 4px;
-    padding: 4px 16px;
-    filter: drop-shadow(4px 4px rgba(0, 0, 0, 0.7));
-  }
+  display: ${props => props.href ? "inline-block" : "none"};
+  color: #000;
+  text-decoration: none;
+  background: #fff;
+  margin: 4px;
+  padding: 4px 16px;
+  filter: drop-shadow(4px 4px rgba(0, 0, 0, 0.7));
 
-  &:hover {
-    filter: drop-shadow(8px 8px rgba(0, 0, 0, 0.7));
-    transform: translate(-2px, -2px);
+  @media (hover: hover) {
+    &:hover {
+      filter: drop-shadow(8px 8px rgba(0, 0, 0, 0.7));
+      transform: translate(-2px, -2px);
+    }
   }
 
   &:active {
