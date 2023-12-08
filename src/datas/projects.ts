fa50d6f2f92775654785891e47070b943c15bc79
aquapaka <aqua.tamlong@gmail.com>
2023-12-08 15:27:22 +0700

    Add project

--- src/datas/projects.ts
+++ src/datas/projects.ts
@@ -6,16 +6,24 @@ export default [
     repositoryUrl: "https://github.com/aquapaka/dotfiles",
   },
   {
+    name: "Make Me Forget",
+    websiteUrl: "https://makemeforget.vercel.app/",
+    downloadUrl: "",
+    repositoryUrl: "",
+  },
+  {
     name: "Low Poly City",
     websiteUrl: "https://aquapaka.github.io/three-ts-city/",
     downloadUrl: "",
-    repositoryUrl: "https://github.com/aquapaka/three-ts-city",
+    repositoryUrl: "",
+    // repositoryUrl: "https://github.com/aquapaka/three-ts-city",
   },
   {
     name: "SuGarden web",
     websiteUrl: "https://aquapaka.github.io/sugarden-webapp/",
     downloadUrl: "",
-    repositoryUrl: "https://github.com/aquapaka/sugarden-webapp",
+    repositoryUrl: "",
+    // repositoryUrl: "https://github.com/aquapaka/sugarden-webapp",
   },
   // {
   //     name: "Flappy Boat Game",
