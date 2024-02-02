f77b75fce70c94acc8640be77bde9d96194aa4d3
aquapaka <aqua.tamlong@gmail.com>
2024-02-02 21:24:04 +0700

    Update project section

--- src/lib/components/DropShadowNormalButton.svelte
+++ src/lib/components/DropShadowNormalButton.svelte
@@ -7,35 +7,19 @@
   $: displayType = href ? "inline-block" : "none";
 </script>
 
-<a {href} {target} style:display={displayType} use:click_sound><slot /></a>
-
-<style>
-  a {
-    width: 80px;
-    text-align: center;
-    color: #000;
-    text-decoration: none;
-    background: #fff;
-    margin: 4px;
-    padding: 4px 16px;
-    filter: drop-shadow(4px 4px rgba(0, 0, 0, 0.5));
-  }
-
-  @media (hover: hover) {
-    a:hover {
-      filter: drop-shadow(8px 8px rgba(0, 0, 0, 0.5));
-      transform: translate(-2px, -2px);
-    }
-  }
-
-  a:active {
-    filter: drop-shadow(2px 2px rgba(0, 0, 0, 0.5));
-    transform: translate(2px, 2px);
-  }
-
-  @media screen and (max-width: 500px) {
-    a {
-      padding: 4px 8px;
-    }
-  }
-</style>
+<div class="group">
+  <a class="
+    text-nowrap
+    px-2 md:px-4 py-1
+    bg-white
+    drop-shadow-[4px_4px_rgba(0,0,0,0.5)]
+    group-hover:drop-shadow-[8px_8px_rgba(0,0,0,0.5)]
+    group-hover:-translate-x-[2px]
+    group-hover:-translate-y-[2px]
+    group-active:drop-shadow-[2px_2px_rgba(0,0,0,0.5)]
+    group-active:translate-x-[2px]
+    group-active:translate-y-[2px]
+    " {href} {target} style:display={displayType} use:click_sound>
+<slot />
+</a>
+</div>
--- src/lib/components/ProjectSection.svelte
+++ src/lib/components/ProjectSection.svelte
@@ -1,31 +1,32 @@
 <script lang="ts">
   import { slide } from "svelte/transition";
   import { bounceOut } from "svelte/easing";
-
   import DropShadowNormalButton from "./DropShadowNormalButton.svelte";
-
   import coralImage from "../../assets/coral-0.png";
-
   import projects from "../../datas/projects";
 
   export let isLoading: boolean;
   export let secretTheme: boolean;
+
   $: backgroundColor = secretTheme ? "#fcda96" : "#60c4f2";
 </script>
 
 {#if !isLoading}
   <div
+    class="pixel-box"
     style:background={backgroundColor}
-    transition:slide={{ delay: 2023, duration: 1000, easing: bounceOut }}
+    transition:slide={{ delay: 2024, duration: 1000, easing: bounceOut }}
   >
     <img class="coral-image" src={coralImage} alt="coral" />
-    <h2>My Projects</h2>
-    <ul>
+    <h2 class="font-bold pb-2">My Projects</h2>
+    <ul class="list-[square] pl-4">
       {#each projects as project}
         <li>
-          <span class="project-item">
+          <div class="flex justify-between align-middle">
+            <span class="project-item">
             {project.name}
-            <span>
+          </span>
+          <span class="pb-2">
               <DropShadowNormalButton href={project.websiteUrl} target="_blank"
                 >View Page</DropShadowNormalButton
               >
@@ -37,7 +38,7 @@
                 target="_blank">Repository</DropShadowNormalButton
               >
             </span>
-          </span>
+          </div>
         </li>
       {/each}
     </ul>
@@ -45,51 +46,6 @@
 {/if}
 
 <style>
-  div {
-    position: relative;
-    background: var(--backgroundColor);
-    max-width: 800px;
-    margin: 24px auto;
-    padding: 12px;
-    -webkit-box-shadow: 0 3px #23313f, 0px -3px #23313f, 3px 0px #23313f,
-      -3px 0px #23313f;
-    box-shadow: 0 3px #23313f, 0px -3px #23313f, 3px 0px #23313f,
-      -3px 0px #23313f;
-  }
-
-  ul {
-    padding: 0 0 0 20px;
-    margin: 12px 0;
-  }
-
-  li {
-    list-style: square;
-  }
-
-  .project-item {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-  }
-
-  span {
-    display: flex;
-  }
-
-  .project-item span {
-    display: flex;
-  }
-
-  h2 {
-    margin: 0;
-  }
-
-  @media screen and (max-width: 512px) {
-    div {
-      padding: 6px;
-    }
-  }
-
   .coral-image {
     width: 100px;
     opacity: 0.5;
