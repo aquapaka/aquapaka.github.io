ae9c8725448fd6c439dc73c2ea9fdbffe477d24b
Tam Long <aqua.tamlong@gmail.com>
2024-05-14 15:19:54 +0700

    Add prettier

--- src/datas/clients.ts
+++ src/datas/clients.ts
@@ -1,13 +1,13 @@
 type client = {
-    name: string;
-    website: string;
-}
+  name: string;
+  website: string;
+};
 
-const clients: client[] =  [
-    {
-        name: "Tiemnghiemtuk",
-        website: "https://tiemnghiemtuk.com"
-    }
-]
+const clients: client[] = [
+  {
+    name: "Tiemnghiemtuk",
+    website: "https://tiemnghiemtuk.com",
+  },
+];
 
 export default clients;
--- src/datas/links.ts
+++ src/datas/links.ts
@@ -5,29 +5,29 @@ import youtubeIcon from "../assets/Youtube.png";
 import discordIcon from "../assets/Discord.png";
 
 export default [
-    {
-        image: emailIcon,
-        url: "mailto:aqua.tamlong@gmail.com",
-        text: "aqua.tamlong@gmail.com",
-    },
-    {
-        image: linkedinIcon,
-        url: "https://www.linkedin.com/in/aquapaka/",
-        text: "linkedin.com/in/aquapaka/",
-    },
-    {
-        image: githubIcon,
-        url: "https://github.com/aquapaka",
-        text: "github.com/aquapaka",
-    },
-    {
-        image: youtubeIcon,
-        url: "https://www.youtube.com/@lanixius",
-        text: "youtube.com/@lanixius",
-    },
-    {
-        image: discordIcon,
-        url: "https://discord.gg/mfMnv6ZFcF",
-        text: "Contact me on discord",
-    },
+  {
+    image: emailIcon,
+    url: "mailto:aqua.tamlong@gmail.com",
+    text: "aqua.tamlong@gmail.com",
+  },
+  {
+    image: linkedinIcon,
+    url: "https://www.linkedin.com/in/aquapaka/",
+    text: "linkedin.com/in/aquapaka/",
+  },
+  {
+    image: githubIcon,
+    url: "https://github.com/aquapaka",
+    text: "github.com/aquapaka",
+  },
+  {
+    image: youtubeIcon,
+    url: "https://www.youtube.com/@lanixius",
+    text: "youtube.com/@lanixius",
+  },
+  {
+    image: discordIcon,
+    url: "https://discord.gg/mfMnv6ZFcF",
+    text: "Contact me on discord",
+  },
 ];
